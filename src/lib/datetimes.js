94100121b8789cae4ef0bf46e60f71c7f53925a7
JC Harrington <jc@brandibble.co>
2022-01-03 18:58:09 -0600

    Revert timeLeft function

--- src/lib/datetimes.js
+++ src/lib/datetimes.js
@@ -53,13 +53,16 @@ export const secondsLeft = (start, end) => {
 }
 
 export const secondsToTime = (seconds) => {
-  let secs = Math.abs(seconds)
-  const hours = Math.floor(secs / 3600)
-  secs = hours > 0 ? secs % 3600 : secs
-  let mins = Math.floor(secs / 60)
-  mins = hours > 0 ? `${mins}`.padStart(2, '0') : mins
-  const s = `${secs % 60}`.padStart(2, '0')
-  return `${seconds < 0 ? '-' : ''}${hours ? `${hours}:` : ''}${mins}:${s}`
+  const mins = Math.floor(seconds / 60)
+  const secs = `${seconds % 60}`.padStart(2, '0')
+  return `${mins}:${secs}`
+  // let secs = Math.abs(seconds)
+  // const hours = Math.floor(secs / 3600)
+  // secs = hours > 0 ? secs % 3600 : secs
+  // let mins = Math.floor(secs / 60)
+  // mins = hours > 0 ? `${mins}`.padStart(2, '0') : mins
+  // const s = `${secs % 60}`.padStart(2, '0')
+  // return `${seconds < 0 ? '-' : ''}${hours ? `${hours}:` : ''}${mins}:${s}`
 }
 
 export const timeLeft = (start, end) => {
